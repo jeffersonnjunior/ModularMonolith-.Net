10dde74cf82d8dcdd28b0aa81373830ded673b34
Jefferson Junior <105082760+jeffersonnjunior@users.noreply.github.com>
2025-04-22 15:41:55 -0300

    Delete Common directory

--- Common/IException/INotificationContext.cs
+++
@@ -1,8 +0,0 @@
-namespace Common.IException;
-
-public interface INotificationContext
-{
-    IReadOnlyCollection<NotificationModel> GetNotifications();
-    void AddNotification(string message);
-    bool HasNotifications();
-}
