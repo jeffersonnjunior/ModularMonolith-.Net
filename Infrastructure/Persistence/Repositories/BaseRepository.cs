17b7f4f404c1196dc658f1a3bc9f020ae33658d8
Jefferson Junior <jefferson.aleluia@cogecom.com.br>
2025-04-24 08:06:13 -0300

    feat: add repository

---
+++ Infrastructure/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,44 @@
+﻿using Common.IPersistence.IRepositories;
+using Infrastructure.Persistence.Context;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class BaseRepository : IBaseRepository
+{
+    private readonly AppDbContext _context;
+
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IQueryable<T> Query<T>() where T : class
+    {
+        return _context.Set<T>();
+    }
+
+    public T? Find<T>(Guid id) where T : class
+    {
+        return _context.Set<T>().Find(id);
+    }
+
+    public void Add<T>(T entity) where T : class
+    {
+        _context.Set<T>().Add(entity);
+    }
+
+    public void Update<T>(T entity) where T : class
+    {
+        _context.Set<T>().Update(entity);
+    }
+
+    public void Remove<T>(T entity) where T : class
+    {
+        _context.Set<T>().Remove(entity);
+    }
+
+    public void SaveChanges()
+    {
+        _context.SaveChanges();
+    }
+}
