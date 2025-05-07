82445ed5c20c049b41ef9ba2ef40dc14ab218385
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-05-07 10:03:23 -0300

    fix: correction CarSaleGetFilterDto

--- Modules/Sales/Dtos/CarSaleDtos/CarSaleGetFilterDto.cs
+++ Modules/Sales/Dtos/CarSaleDtos/CarSaleGetFilterDto.cs
@@ -7,4 +7,6 @@ public class CarSaleGetFilterDto
     public Guid ProductionOrderIdEqual { get; set; }
     public SaleStatus StatusEqual { get; set; }
     public DateTime? SoldAtEqual { get; set; }
+    public int PageSize { get; set; } = 10;
+    public int PageNumber { get; set; } = 1;
 }
